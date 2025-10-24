f396a4a9d211ab3439c9e9a2a19f382f325516fb
Matheus <masantos1007@gmail.com>
2025-10-24 10:01:36 -0300

    AtualizacoesGerais/ProjetoFinalizadoPrimeiraVersao

--- ANSYS.Infrastructure/Dependency/DependencyInjection.cs
+++ ANSYS.Infrastructure/Dependency/DependencyInjection.cs
@@ -1,14 +1,12 @@
 ﻿using ANSYS.Application.Global.PedidoItens.Mappers;
 using ANSYS.Application.Global.Pedidos.Mappers;
 using ANSYS.Application.Global.Usuarios.Mappers;
-using ANSYS.Domain.Abstractions.Context;
 using ANSYS.Domain.Abstractions.Context.EntityFramework;
 using ANSYS.Domain.Global.PedidoItens.Repositories;
 using ANSYS.Domain.Global.Pedidos.Repositories;
 using ANSYS.Domain.Global.Usuarios.Repositories;
 using ANSYS.Infrastructure.Context;
 using ANSYS.Infrastructure.Context.EntityFramework;
-using ANSYS.Infrastructure.Context.Local;
 using ANSYS.Infrastructure.Global.PedidoItens;
 using ANSYS.Infrastructure.Global.Pedidos;
 using ANSYS.Infrastructure.Global.Usuarios;
@@ -25,8 +23,6 @@ namespace ANSYS.Dependency.Infrastructure
             IConfiguration configuration)
         {
             AddPersistence(services, configuration);
-
-            //AddAuthentication(services, configuration);
 
             return services;
         }
@@ -52,42 +48,5 @@ namespace ANSYS.Dependency.Infrastructure
             //Configuração de qual banco de dados será usado...
             DatabaseRunModeConfiguration.UseLocalDatabase(false);
         }
-
-        //Configuração para gerar Token a partir do jwt bearer
-
-        //private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
-        //{
-        //    //AppSettings
-        //    services.AddSingleton<IAppAuthSettings>(configuration.GetSection("AuthSettings").Get<AppAuthSettings>()!);
-
-        //    // Obtém a seção "AppSettings" do arquivo de configuração
-        //    var appSettingsSection = configuration.GetSection("AuthSettings");
-
-        //    //// Configura a classe AppSettings para ser injetada via DI
-        //    services.Configure<AppAuthSettings>(appSettingsSection);
-
-        //    // Obtém a instância de AppSettings
-        //    var appSettings = (appSettingsSection.Get<AppAuthSettings>()!);
-
-        //    // Converte a chave secreta para um array de bytes
-        //    var key = Encoding.ASCII.GetBytes(appSettings.Segredo);
-
-        //    // Configura o serviço de autenticação JWT
-        //    services.AddAuthentication(options =>
-        //    {
-        //        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        //        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        //    })
-        //    .AddJwtBearer(options =>
-        //    {
-        //        options.TokenValidationParameters = new TokenValidationParameters
-        //        {
-        //            ValidateIssuerSigningKey = true,
-        //            IssuerSigningKey = new SymmetricSecurityKey(key), // Usa a chave secreta do AppAuthSettings
-        //            ValidateIssuer = false,
-        //            ValidateAudience = false,
-        //        };
-        //    });
-        //}
     }
 }
